22e9a0d8a1fba6db53db256035231fa3c6a95bfd
BENEDICT PEH JIA WEI 2305071I <2305071I@student.tp.edu.sg>
2024-11-18 17:10:05 +0800

    Added dataase and seed data

---
+++ CarRentalManagement/Domain/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
+
+namespace CarRentalManagement.Domain
+{
+	public class Vehicle : BaseDomainModel
+	{
+		public int Year { get; set; }
+		public string? LicensePLateNumeber { get; set; }
+		public int MakeId { get; set; }
+		public int ModelId { get; set; }
+		public int ColorId { get; set; }
+	}
+}
